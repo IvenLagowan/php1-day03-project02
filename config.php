c7c8265a3dc1dcb3d6080340ab83ce4d2141671b
Iven DF <94290469+ivendf@users.noreply.github.com>
2022-05-30 04:14:41 -0700

    final project02

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+	$judul = "SaCode's Weekend";
+	$tagLine = "#SaCode #Papuancoders";
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	include('config.php');
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title><<?=$judul ?></title>
+</head>
+<body>
+	<b><?=$tagLine ?></b>
+</body>
+</html>
